d13029722c6f2c60527330c610fa2cd1e7538433
codeamac02 <codeamac02@gmail.com>
2015-06-03 13:21:39 -0500

    Metodo Iterativo

---
+++ Factorial.rb
@@ -0,0 +1,30 @@
+def factorial_iterative(n)
+	# r = n * (n+1) 
+	# r = r * (n+2)
+	# r = r * (n+3)
+	# r = r * (n+4)
+	r = 1
+	for i in (1...n)
+	   r = r * (i+1)
+		 # puts "r:#{r} = i:#{i} * (i+1):#{i+1}"
+	end
+	r
+end
+
+# def factorial_iterative(n)
+# 	(1..n).inject(&:*)
+# end
+
+puts factorial_iterative(5) == 120
+
+p factorial_iterative(5) == 120
+# # p factorial_recursive(5) == 120
+
+p factorial_iterative(0) == 1
+# # p factorial_recursive(0) == 1
+
+p factorial_iterative(2) == 2
+# # p factorial_recursive(2) == 2
+
+p factorial_iterative(8) == 40_320
+# # p factorial_recursive(8) == 40_320
