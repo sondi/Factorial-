cae0fe93a1becac5471be3e745b2ddf734159a47
codeamac02 <codeamac02@gmail.com>
2015-06-03 13:58:02 -0500

    Metodo Factorial iterativo y recursivo

--- Factorial.rb
+++ Factorial.rb
@@ -1,19 +1,24 @@
-# def factorial_iterative(n)
-	# r = n * (n+1) 
-	# r = r * (n+2)
-	# r = r * (n+3)
-	# r = r * (n+4)
-# 	r = 1
-# 	for i in (1...n)
-# 	   r = r * (i+1)
-# 		 # puts "r:#{r} = i:#{i} * (i+1):#{i+1}"
-# 	end
-# 	r
-# end
+#METODO ITERATIVO 2
 
-# def factorial_iterative(n)
-# 	(1..n).inject(&:*)
-# end
+def factorial_iterative(n)
+	r = n * (n+1) 
+	r = r * (n+2)
+	r = r * (n+3)
+	r = r * (n+4)
+	r = 1
+	for i in (1...n)
+	   r = r * (i+1)
+		 # puts "r:#{r} = i:#{i} * (i+1):#{i+1}"
+	end
+	r
+end
+
+#METODO ITERATIVO 2
+def factorial_iterative2(n)
+	(1..n).inject(&:*)
+end
+
+#METODO RECURSIVO
 def factorial_recursive(n)
 	if n == 0
 		1
@@ -24,14 +29,18 @@ end
 
 
 
-# p factorial_iterative(5) == 120
+p factorial_iterative(5) == 120
+p factorial_iterative2(5) == 120
 p factorial_recursive(5) == 120
 
-# p factorial_iterative(0) == 1
+p factorial_iterative(0) == 1
+p factorial_iterative2(0) == 1
 p factorial_recursive(0) == 1
 
-# p factorial_iterative(2) == 2
+p factorial_iterative(2) == 2
+p factorial_iterative2(2) == 2
 p factorial_recursive(2) == 2
 
-# p factorial_iterative(8) == 40_320
+p factorial_iterative(8) == 40_320
+p factorial_iterative2(8) == 40_320
 p factorial_recursive(8) == 40_320
